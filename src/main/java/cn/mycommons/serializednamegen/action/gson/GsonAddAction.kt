c9d46e90d89b9ed2e8ad079dcd4e9a9a9ea2f674
leonxia <leonxia@vipabc.om>
2018-10-18 20:23:41 +0800

    update

--- src/main/java/cn/mycommons/serializednamegen/action/gson/GsonAddAction.kt
+++ src/main/java/cn/mycommons/serializednamegen/action/gson/GsonAddAction.kt
@@ -10,7 +10,6 @@ import com.intellij.psi.PsiClass
 import com.intellij.psi.PsiElement
 import com.intellij.psi.PsiFile
 import com.intellij.psi.PsiJavaFile
-import org.jetbrains.kotlin.idea.refactoring.fqName.getKotlinFqName
 import org.jetbrains.kotlin.psi.KtClass
 import org.jetbrains.kotlin.psi.KtClassBody
 import org.jetbrains.kotlin.psi.KtFile
@@ -35,14 +34,9 @@ class GsonAddAction : BaseAction() {
         // 处理内部类
         clazz.children
                 .filter {
-                    println("PsiFile2 -> $psiFile")
-                    println("it2 -> $it")
-                    it is PsiClass
-                            || it is KtClass
-                            // || it is KtClassBody
+                    it is PsiClass || it is KtClass || it is KtClassBody
                 }
                 .forEach {
-                    print("it->${it.getKotlinFqName()}")
                     doModify(project, psiFile, fileType, it)
                 }
     }
